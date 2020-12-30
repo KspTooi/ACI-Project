0cd08f4d38cc808e9a8cce3e83d000def8c25253
ksptooi <ksptooi@protonmail.ch>
2020-12-30 21:50:54 +0800

    Target生成器

--- acu-main/src/main/kotlin/com/ksptooi/acu/service/TargetService.kt
+++ acu-main/src/main/kotlin/com/ksptooi/acu/service/TargetService.kt
@@ -2,9 +2,12 @@ package com.ksptooi.acu.service
 
 interface TargetService {
 
+    fun forName(name: String):Target
+
     fun forEngine(name:String):Target
 
     fun forFile(path:String):Target
 
-
+    fun forDirectory(path: String):Target
+    
 }
